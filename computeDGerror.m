a43e2478b5515496d0a63d5d42dba629c268fd9a
jongif7270 <jongif@ajou.ac.kr>
2023-12-06 04:13:47 +0900

    202312060413

--- computeDGerror.m
+++ computeDGerror.m
@@ -7,8 +7,8 @@ ys = (c4n(n4e(:,2),2)-c4n(n4e(:,3),2))/2;
 J = xr.*ys-xs.*yr;
 rx=ys./J; ry=-xs./J; sx=-yr./J; sy=xr./J;
 I = eye((N+1)*(N+2)/2);
-M=I/(V*V');
 for j=1:size(n4e,1)
+M=J(j)*I/(V*V');
 Dx_u = (rx(j)*Dr+sx(j)*Ds)*u(ind4e(j,:));
 Dy_u = (ry(j)*Dr+sy(j)*Ds)*u(ind4e(j,:));
 Dex=ux(c4n2(ind4e(j,:),:)) - Dx_u;
