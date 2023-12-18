95310f4c13cc507a895fd03600be11984d2d0adb
Yang Chang su <jongif@ajou.ac.kr>
2023-12-18 18:06:57 +0900

    202312181806

--- HDG2.m
+++ HDG2.m
@@ -3,7 +3,7 @@ function [A,B,C,D,E,b,u] = HDG2(M,N)
 xl=0;xr=1;yl=0;yr=1;Mx=M;My=M;
 
 [c4n,n4e,~,~] = mesh_fem_2d_triangle(xl,xr,yl,yr,Mx,My,N);
-[ind4e,~,inddb,c4n2,~] = indexforDG(xl,xr,yl,yr,Mx,My,N);
+[ind4e,~,~,c4n2,~] = indexforDG(xl,xr,yl,yr,Mx,My,N);
 
 b = zeros(size(ind4e(:),1),1);
 u = b;
@@ -57,7 +57,20 @@ B=zeros(Mx*My*(N+1)*(N+2),size(e4s,1)*(N+1));
 D=zeros(size(e4s,1)*(N+1),size(e4s,1)*(N+1));
 
 en=mod(ind4s(:,:,:)-1,(N+1)*(N+2)/2)+1;
-k=4*N^8;
+k=4*N^2;
+
+for j=1:size(n4e,1)
+    for i=1:3
+        n=normal(c4n2(ind4s(s4e(j,i),N+1,1),:)-c4n2(ind4s(s4e(j,i),1,1),:));
+        h=norm(c4n2(ind4s(s4e(j,i),N+1,1),:)-c4n2(ind4s(s4e(j,i),1,1),:));
+        M=I1D/(V1D*V1D');
+        A(ind4s(s4e(j,i),:,1),ind4s(s4e(j,i),:,1))=A(ind4s(s4e(j,i),:,1),ind4s(s4e(j,i),:,1))+(h/2)*(k/(sqrt(2)*(1/Mx)))*M;
+
+        A(ind4e(j,:),ind4s(s4e(j,i),:,1))=A(ind4e(j,:),ind4s(s4e(j,i),:,1))+(h/2)*((rx(j)*Dr(en(j,:,1),:)'*M+sx(j)*Ds(en(j,:,1),:)'*M)*n(1)+(ry(j)*Dr(en(j,:,1),:)'*M+sy(j)*Ds(en(j,:,1),:)'*M)*n(2));
+        A(ind4s(s4e(j,i),:,1),ind4e(j,:))=A(ind4s(s4e(j,i),:,1),ind4e(j,:))+(h/2)*((rx(j)*M*Dr(en(j,:,1),:)+sx(j)*M*Ds(en(j,:,1),:))*n(1)+(ry(j)*M*Dr(en(j,:,1),:)+sy(j)*M*Ds(en(j,:,1),:))*n(2));
+    end
+end
+
 for j=1:size(n4e,1)
     for i=1:3
         n=normal(c4n2(ind4s(s4e(j,i),N+1,1),:)-c4n2(ind4s(s4e(j,i),1,1),:));
@@ -72,7 +85,6 @@ C=B';
 
 for j=1:size(n4e,1)
     for i=1:3
-        n=normal(c4n2(ind4s(s4e(j,i),N+1,1),:)-c4n2(ind4s(s4e(j,i),1,1),:));
         h=norm(c4n2(ind4s(s4e(j,i),N+1,1),:)-c4n2(ind4s(s4e(j,i),1,1),:));
         M=I1D/(V1D*V1D');
         D((s4e(j,i)-1)*(N+1)+(1:N+1),(s4e(j,i)-1)*(N+1)+(1:N+1))=D((s4e(j,i)-1)*(N+1)+(1:N+1),(s4e(j,i)-1)*(N+1)+(1:N+1))+(h/2)*(k/(sqrt(2)*(1/Mx)))*M;
@@ -80,6 +92,8 @@ for j=1:size(n4e,1)
 end
 
 E=[A B; C D];
+
+spy(E);
 
 V=-(D-(C/A)*B)\(C/A)*b;
 u=A\(b-B*V);
